8b97c6e2d2ed405387d2096afeebff65f18d5ebc
chaojibaozi <397473141@qq.com>
2026-07-26 23:13:40 +0800

    修复

--- Keyword-Segmentation-Tool/build/bench2.mjs
+++
@@ -1,7 +0,0 @@
-import { api } from './test_seg_audit.mjs';
-api.buildRegexCache?.();
-const kws=[]; for(let i=0;i<50000;i++) kws.push('测试词'+(i%2000)+'空调论坛');
-const t0=Date.now();
-let c=0; for(const k of kws){ const r=api.classify(k); if(r&&r.cat) c++; }
-const t1=Date.now();
-console.log('classify() single-call', c, 'in',(t1-t0),'ms =>',Math.round(c/((t1-t0)/1000)),'kw/s');
